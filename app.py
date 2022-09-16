c55991c2444c3d7d25686ff168a4a57c6c4666be
vivian <vivian.v18723@gmail.com>
2022-09-16 19:58:41 +0100

    add favicon & get css working

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return "Welcome to the flower factory!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
